04e98609bcc129849ed4e40f78b585c3411115d3
hari <krishnadude98@gmail.com>
2021-03-27 13:07:26 +0530

    CVUD handling added

--- src/controller/share.js
+++ src/controller/share.js
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 
 export default({ config, db }) => {
   let api = Router();
-
+  //CRUD Create Read Update Delete
   // '/v1/restaurant/add'
   api.post('/add', (req, res) => {
     let newRide = new ShareRide();
@@ -24,5 +24,60 @@ export default({ config, db }) => {
       res.json({ message: 'Advertisement added sucessfuly' });
     });
   });
+  //v1/share --Read
+  api.get('/',(req,res)=>{
+    ShareRide.find({},(err,shareRides)=>{
+      if(err){
+        res.send(err);
+      }
+      res.json(shareRides);
+    });
+  });
+
+  // v1/share/:id --Read 1
+  api.get('/:id',(req,res)=>{
+    ShareRide.findById(req.params.id,(err,shareRide)=>{
+      if(err){
+        res.send(err);
+      }
+      res.json(shareRide);
+    });
+  });
+  // v1/share/:id --Update
+  api.put('/:id',(req,res)=>{
+    ShareRide.findById(req.params.id,(err,shareRide)=>{
+      if(err){
+        res.send(err);
+      }
+      shareRide.from= req.body.from;
+      shareRide.to= req.body.to;
+      shareRide.date = req.body.date;
+      shareRide.time = req.body.time;
+      shareRide.vehicleno = req.body.vehicleno;
+      shareRide.vehiclemodel = req.body.vehiclemodel;
+      shareRide.eamount = req.body.eamount;
+
+      shareRide.save(err=>{
+        if(err){
+          res.send(err);
+        }
+        res.json({message:"Advertisement info updated"});
+      });
+    });
+  });
+
+  //v1/share/:// IDEA:
+    api.delete('/:id',(req,res)=>{
+      ShareRide.remove({
+        _id:req.params.id
+      },(err,share)=>{
+        if(err){
+          res.send(err);
+        }
+        res.json({messgae:"Advertisement sucessfully removed"});
+      });
+
+    });
+
   return api;
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import config from '../config';
 import initializeDb from '../db';
 import middleware from '../middleware';
-import restaurant from '../controller/share';
+import share from '../controller/share';
 
 let router = express();
 
@@ -13,7 +13,7 @@ initializeDb(db => {
   router.use(middleware({ config, db }));
 
   // api routes v1 (/v1)
-  router.use('/share', restaurant({ config, db }));
+  router.use('/share', share({ config, db }));
 });
 
 export default router;
