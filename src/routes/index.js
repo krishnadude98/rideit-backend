0dd43f6d2f2f26519dc97c8251cfcbe07481d5c4
hari <krishnadude98@gmail.com>
2021-04-01 13:39:53 +0530

    user registration is working

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ import initializeDb from '../db';
 import middleware from '../middleware';
 import share from '../controller/share';
 import rentrider from '../controller/rentrider';
-import rentride from '../controller/rentride'
+import rentride from '../controller/rentride';
+import account from '../controller/account';
 let router = express();
 
 // connect to db
@@ -17,6 +18,7 @@ initializeDb(db => {
   router.use('/share', share({ config, db }));
   router.use('/rentrider', rentrider({ config, db }));
   router.use('/rentride', rentride({ config, db }));
+  router.use('/account', account({ config, db }));
 });
 
 export default router;
